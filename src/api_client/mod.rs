06390237c4b788e4da66ec88b39f44a25c8174c6
ljoonal <ljoonal@ljoonal.xyz>
2023-01-24 21:41:23 +0200

    Split auth and unauth API client

--- src/api_client/mod.rs
+++ src/api_client/mod.rs
@@ -80,8 +80,19 @@ impl From<tokio_tungstenite::tungstenite::Error> for ApiError {
 type NormalRateLimiter =
 	RateLimiter<NotKeyed, InMemoryState, DefaultClock, NoOpMiddleware>;
 
-/// The main API client
-pub struct CVR {
+/// The main API client without authentication
+#[cfg(feature = "http_client")]
+pub struct UnauthenticatedCVR {
+	user_agent: String,
+	#[cfg(feature = "http_client")]
+	http: Client,
+	#[cfg(feature = "http_client")]
+	http_rate_limiter: NormalRateLimiter,
+}
+
+/// The main API client with authentication
+pub struct AuthenticatedCVR {
+	user_agent: String,
 	#[cfg(feature = "http_client")]
 	http: Client,
 	#[cfg(feature = "http_client")]
@@ -89,97 +100,143 @@ pub struct CVR {
 	#[cfg(feature = "ws_client")]
 	ws: tokio::sync::RwLock<Option<ws::Client>>,
 	#[cfg(feature = "ws_client")]
-	user_agent: String,
-	#[cfg(feature = "ws_client")]
 	auth: SavedLoginCredentials,
 }
 
 #[doc(hidden)]
 #[cfg(feature = "http_client")]
-pub enum SupportedApiStates {
+pub enum AuthenticatedSupportedApiStates {
 	Unauthenticated,
 	Authenticated(SavedLoginCredentials),
 }
 
 #[cfg(feature = "http_client")]
-impl From<NoAuthentication> for SupportedApiStates {
+impl From<NoAuthentication> for AuthenticatedSupportedApiStates {
 	fn from(_: NoAuthentication) -> Self {
 		Self::Unauthenticated
 	}
 }
 
 #[cfg(feature = "http_client")]
-impl From<SavedLoginCredentials> for SupportedApiStates {
+impl From<SavedLoginCredentials> for AuthenticatedSupportedApiStates {
 	fn from(auth: SavedLoginCredentials) -> Self {
 		Self::Authenticated(auth)
 	}
 }
 
-impl CVR {
+#[cfg(feature = "http_client")]
+async fn base_query<R, FromState, T>(
+	http: &Client,
+	rate_limiter: &NormalRateLimiter,
+	queryable: T,
+) -> Result<R, ApiError>
+where
+	R: DeserializeOwned,
+	T: Queryable<FromState, R> + Send + Sync,
+{
+	let mut request = http.request(
+		match queryable.method() {
+			RequestMethod::Get => reqwest::Method::GET,
+			RequestMethod::Head => reqwest::Method::HEAD,
+			RequestMethod::Patch => reqwest::Method::PATCH,
+			RequestMethod::Post => reqwest::Method::POST,
+			RequestMethod::Put => reqwest::Method::PUT,
+			RequestMethod::Delete => reqwest::Method::DELETE,
+		},
+		queryable.url(),
+	);
+	if let Some(body) = queryable.body() {
+		request = request.body(body?);
+	}
+
+	rate_limiter.until_ready().await;
+	let response = request.send().await?.error_for_status()?;
+	// TODO: Figure out if there are any extra rate limit headers to respect
+
+	#[cfg(feature = "debug")]
+	{
+		let text = response.text().await?;
+		dbg!(&text);
+		Ok(serde_json::from_str::<R>(&text)?)
+	}
+	#[cfg(not(feature = "debug"))]
+	{
+		Ok(response.json::<R>().await?)
+	}
+}
+
+#[cfg(feature = "http_client")]
+#[must_use]
+fn http_rate_limiter() -> NormalRateLimiter {
+	// ~5 seconds per request sustained over one minute, allowing up to a request
+	// per second in bursts.
+	RateLimiter::direct(
+		Quota::per_minute(NonZeroU32::try_from(12).unwrap())
+			.allow_burst(NonZeroU32::try_from(5).unwrap()),
+	)
+}
+
+impl AuthenticatedCVR {
 	/// Creates an API client
 	#[cfg(feature = "http_client")]
 	fn http_client(
 		user_agent: &str,
-		auth: &Option<SavedLoginCredentials>,
+		auth: &SavedLoginCredentials,
 	) -> Result<Client, ApiError> {
 		use serde::ser::Error;
 
 		let builder = Client::builder();
 		let mut headers = HeaderMap::new();
 
-		if let Some(auth) = auth {
-			headers.insert(
-				"Username",
-				auth.username.parse().map_err(|_| {
-					serde_json::Error::custom("Couldn't turn username into a header")
-				})?,
-			);
-			headers.insert(
-				"AccessKey",
-				auth.access_key.parse().map_err(|_| {
-					serde_json::Error::custom("Couldn't turn access_key into a header")
-				})?,
-			);
-		}
+		headers.insert(
+			"Username",
+			auth.username.parse().map_err(|_| {
+				serde_json::Error::custom("Couldn't turn username into a header")
+			})?,
+		);
+		headers.insert(
+			"AccessKey",
+			auth.access_key.parse().map_err(|_| {
+				serde_json::Error::custom("Couldn't turn access_key into a header")
+			})?,
+		);
 
 		Ok(builder.user_agent(user_agent).default_headers(headers).build()?)
 	}
 
-	/// Creates the rate limiter for the HTTP API
-	#[cfg(feature = "http_client")]
-	#[must_use]
-	fn http_rate_limiter() -> NormalRateLimiter {
-		RateLimiter::direct(
-			Quota::per_minute(NonZeroU32::try_from(12).unwrap())
-				.allow_burst(NonZeroU32::try_from(5).unwrap()),
-		)
-	}
-
-	/// Creates a new CVR API client
+	/// Removes authentication to the API client
 	///
 	/// # Errors
 	///
-	/// If deserializing user agent into a header fails,
-	/// or if WS API is enabled & the connection establishment fails.
+	/// If deserializing user agent fails.
+	#[cfg(feature = "http_client")]
+	pub fn downgrade(self) -> Result<UnauthenticatedCVR, ApiError> {
+		Ok(UnauthenticatedCVR {
+			http: UnauthenticatedCVR::http_client(&self.user_agent)?,
+			http_rate_limiter: self.http_rate_limiter,
+			user_agent: self.user_agent,
+		})
+	}
+
+	/// Creates a new authenticated CVR API client
+	///
+	/// # Errors
+	///
+	/// If deserializing user agent into a header fails
 	pub fn new(
 		user_agent: String,
-		#[cfg(not(feature = "ws_client"))] auth: impl Into<Option<SavedLoginCredentials>>
-		+ Send,
-		#[cfg(feature = "ws_client")] auth: impl Into<SavedLoginCredentials> + Send,
+		auth: impl Into<SavedLoginCredentials> + Send,
 	) -> Result<Self, ApiError> {
 		let auth = auth.into();
 		Ok(Self {
 			#[cfg(feature = "http_client")]
-			http: Self::http_client(&user_agent, &auth.clone().into())?,
-			// ~5 seconds per request sustained over one minute, allowing up to a request
-			// per second in bursts.
+			http: Self::http_client(&user_agent, &auth)?,
 			#[cfg(feature = "http_client")]
-			http_rate_limiter: Self::http_rate_limiter(),
+			http_rate_limiter: http_rate_limiter(),
 			#[cfg(feature = "ws_client")]
 			ws: tokio::sync::RwLock::new(None),
 			#[cfg(feature = "ws_client")]
 			auth,
-			#[cfg(feature = "ws_client")]
 			user_agent,
 		})
 	}
@@ -200,7 +257,7 @@ impl CVR {
 	) -> Result<ReturnType, ApiError>
 	where
 		WrappedType: CvrApiUnwrapping<ReturnType> + DeserializeOwned,
-		FromState: Into<SupportedApiStates>,
+		FromState: Into<AuthenticatedSupportedApiStates>,
 		T: Queryable<FromState, WrappedType> + Send + Sync,
 	{
 		Ok(self.query_without_unwrapping(queryable).await?.unwrap_data())
@@ -218,38 +275,10 @@ impl CVR {
 	) -> Result<R, ApiError>
 	where
 		R: DeserializeOwned,
-		FromState: Into<SupportedApiStates>,
+		FromState: Into<AuthenticatedSupportedApiStates>,
 		T: Queryable<FromState, R> + Send + Sync,
 	{
-		let mut request = self.http.request(
-			match queryable.method() {
-				RequestMethod::Get => reqwest::Method::GET,
-				RequestMethod::Head => reqwest::Method::HEAD,
-				RequestMethod::Patch => reqwest::Method::PATCH,
-				RequestMethod::Post => reqwest::Method::POST,
-				RequestMethod::Put => reqwest::Method::PUT,
-				RequestMethod::Delete => reqwest::Method::DELETE,
-			},
-			queryable.url(),
-		);
-		if let Some(body) = queryable.body() {
-			request = request.body(body?);
-		}
-
-		self.http_rate_limiter.until_ready().await;
-		let response = request.send().await?.error_for_status()?;
-		// TODO: Figure out if there are any extra rate limit headers to respect
-
-		#[cfg(feature = "debug")]
-		{
-			let text = response.text().await?;
-			dbg!(&text);
-			Ok(serde_json::from_str::<R>(&text)?)
-		}
-		#[cfg(not(feature = "debug"))]
-		{
-			Ok(response.json::<R>().await?)
-		}
+		base_query(&self.http, &self.http_rate_limiter, queryable).await
 	}
 
 	/// Sends a WS message to the CVR API.
@@ -306,5 +335,91 @@ impl CVR {
 			.as_ref()
 			.expect("client should exist as lock was never dropped")
 			.listen())
+	}
+}
+
+impl UnauthenticatedCVR {
+	/// Creates an unauthenticated API client
+	#[cfg(feature = "http_client")]
+	fn http_client(user_agent: &str) -> Result<Client, ApiError> {
+		Ok(Client::builder().user_agent(user_agent).build()?)
+	}
+
+	/// Adds authentication to the API client
+	///
+	/// # Errors
+	///
+	/// If deserializing user agent or authentication fails.
+	pub fn upgrade(
+		self,
+		auth: impl Into<SavedLoginCredentials> + Send,
+	) -> Result<AuthenticatedCVR, ApiError> {
+		let auth = auth.into();
+		Ok(AuthenticatedCVR {
+			#[cfg(feature = "http_client")]
+			http: AuthenticatedCVR::http_client(&self.user_agent, &auth)?,
+			#[cfg(feature = "http_client")]
+			http_rate_limiter: self.http_rate_limiter,
+			#[cfg(feature = "ws_client")]
+			ws: tokio::sync::RwLock::new(None),
+			#[cfg(feature = "ws_client")]
+			auth,
+			user_agent: self.user_agent,
+		})
+	}
+
+	/// Creates a new CVR API client
+	///
+	/// # Errors
+	///
+	/// If deserializing user agent into a header fails,
+	/// or if WS API is enabled & the connection establishment fails.
+	#[cfg(feature = "http_client")]
+	pub fn new(user_agent: String) -> Result<Self, ApiError> {
+		Ok(Self {
+			http: Self::http_client(&user_agent)?,
+			http_rate_limiter: http_rate_limiter(),
+			user_agent,
+		})
+	}
+
+	/// Sends a query to the CVR API
+	///
+	/// Also automatically unwraps the data field, discarding the message.
+	/// Use [`cvr.query_without_unwrapping`](Self::query_without_unwrapping) if
+	/// you want to access the message field too.
+	///
+	/// # Errors
+	///
+	/// If something with the request failed.
+	#[cfg(feature = "http_client")]
+	pub async fn query<ReturnType, WrappedType, FromState, T>(
+		&self,
+		queryable: T,
+	) -> Result<ReturnType, ApiError>
+	where
+		WrappedType: CvrApiUnwrapping<ReturnType> + DeserializeOwned,
+		FromState: Into<NoAuthentication>,
+		T: Queryable<FromState, WrappedType> + Send + Sync,
+	{
+		Ok(self.query_without_unwrapping(queryable).await?.unwrap_data())
+	}
+
+	/// Sends a query to the CVR API
+	///
+	/// # Errors
+	///
+	/// If something with the request failed.
+	#[cfg(feature = "http_client")]
+	pub async fn query_without_unwrapping<R, FromState, T>(
+		&self,
+		queryable: T,
+	) -> Result<R, ApiError>
+	where
+		R: DeserializeOwned,
+		FromState: Into<NoAuthentication>,
+		T: Queryable<FromState, R> + Send + Sync,
+	{
+		base_query(&self.http, &self.http_rate_limiter, queryable).await
 	}
 }
--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -2,7 +2,7 @@
 // Something's funky with checking if these are used or not.
 #![allow(dead_code)]
 use chilloutvr::{
-	api_client::CVR,
+	api_client::AuthenticatedCVR,
 	model::{ResponseDataWrapper, UserAuth},
 	query::SavedLoginCredentials,
 };
@@ -15,7 +15,7 @@ const USER_AGENT: &str = concat!(
 	") - tests",
 );
 
-pub fn api_client() -> CVR {
+pub fn api_client() -> AuthenticatedCVR {
 	let user_auth: UserAuth = serde_json::from_slice::<ResponseDataWrapper<UserAuth>>(
 		&std::fs::read("user-auth.json")
 			.expect("must have a prepared `user-auth.json` file for live API testing"),
@@ -26,5 +26,6 @@ pub fn api_client() -> CVR {
 	assert!(!user_auth.username.is_empty());
 	assert!(user_auth.access_key.len() > 20);
 
-	CVR::new(USER_AGENT.to_owned(), SavedLoginCredentials::from(user_auth)).unwrap()
+	AuthenticatedCVR::new(USER_AGENT.to_owned(), SavedLoginCredentials::from(user_auth))
+		.unwrap()
 }
